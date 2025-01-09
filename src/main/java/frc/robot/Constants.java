9e27a89e13f5bf3db58b73f5fbd7866121fc2574
Lucas Jin <kblazer20@gmail.com>
2025-01-09 14:14:12 -0500

    Added vision subsystem with pipeline and pose estimation in regards to april tag

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,17 @@
 
 package frc.robot;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.math.Matrix;
+
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -16,4 +27,29 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+
+  public static class VisionConstants {
+    public static final String CAMERA_NAME = "photonvision_camera";
+
+    // April Tag Layout for Pose Estimator
+    public static final AprilTagFieldLayout TAG_LAYOUT = AprilTagFields.kDefaultField.loadAprilTagLayoutField();
+
+    public static final int CAMERA_ROLL = 0;
+    public static final int CAMERA_PITCH = 0;
+    public static final double CAMERA_YAW = Math.PI;
+
+
+    // Where the camera is mounted with regards to the robot
+    public static final Transform3d ROBOT_TO_CAM = new Transform3d(new Translation3d(0, 0, 0),
+      new Rotation3d(CAMERA_ROLL, CAMERA_PITCH * Math.PI/180, CAMERA_YAW));
+        // Translation3d is offset from center or robot/robot origin
+        // Rotation3d is rotation on forward/back axis, rotation on left/right axis, rotation on up/down axis
+
+    // Set values for std devs to determine trustworthiness
+      // Change with actual values in future
+    public static final Matrix<N3, N1> SINGLE_TAG_SD = VecBuilder.fill(0, 0, 0); // Should be higher
+    public static final Matrix<N3, N1> MULTI_TAG_SD = VecBuilder.fill(0, 0, 0);
+  }
+
+
 }
---
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -0,0 +1,133 @@
+package frc.robot.subsystems;
+
+import static frc.robot.Constants.VisionConstants.*;
+
+import java.util.AbstractMap;
+import java.util.Optional;
+
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+// Links:
+    // https://docs.photonvision.org/en/latest/docs/programming/photonlib/robot-pose-estimator.html
+    // https://docs.photonvision.org/en/v2025.0.0-beta-8/docs/programming/photonlib/getting-target-data.html
+
+public class VisionSubsystem extends SubsystemBase{
+    private final PhotonCamera camera; // Change camera name later
+    private final PhotonPoseEstimator poseEstimator;
+    private double lastEstTime;
+
+    public VisionSubsystem() { // Constructor
+        camera = new PhotonCamera(CAMERA_NAME);
+        
+        poseEstimator = new PhotonPoseEstimator(
+            TAG_LAYOUT, 
+            PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, 
+            ROBOT_TO_CAM
+        );
+        poseEstimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY); // What to use if only 1 tag found
+    }
+
+    // Returns an estimated pose, estimate timestamp, and targets used for estimation.
+    public AbstractMap.SimpleEntry<Optional<EstimatedRobotPose>, Pose3d> getEstimatedGlobalPose() {
+        // Get pipline result
+        var result = camera.getLatestResult();
+
+        // Update pose estimator with result
+        Optional<EstimatedRobotPose> visionEst = poseEstimator.update(result);
+        // Only new results when new time
+        double latestTime = camera.getLatestResult().getTimestampSeconds();
+        boolean newResult = Math.abs(latestTime - lastEstTime) > 1e-5;
+        if (newResult)
+            lastEstTime = latestTime;
+
+        Pose3d estimatedPose = poseEstimator.getReferencePose();
+        return new AbstractMap.SimpleEntry<>(visionEst, estimatedPose);
+    }
+
+    // Returns standard deviations (essentially trustworthiness) of the estimated pose (to be used alongside swerve to align robot)
+    public Matrix<N3, N1> getEstimationStdDevs(Pose2d estimatedPose) {
+        var estStdDevs = SINGLE_TAG_SD;
+        var targets = camera.getLatestResult().getTargets();
+        int numTags = 0;
+        double avgDist = 0;
+
+        // Loop through all detected targets
+        for (var target: targets) {
+            // Get pose (position + orientation)
+            var tagPose = poseEstimator.getFieldTags().getTagPose(target.getFiducialId());
+            if (tagPose.isEmpty())
+                continue;
+            numTags++;
+            // Distance between tag and estimated robot pose
+            avgDist += tagPose.get().toPose2d().getTranslation().getDistance(estimatedPose.getTranslation());
+        }
+
+        if (numTags == 0)
+            return estStdDevs;
+        // Average distance
+        avgDist /= numTags;
+        // If more than 1 tag -> need to use a more trusting SD
+        if (numTags > 1)
+            estStdDevs = MULTI_TAG_SD;
+        // If only 1 tag and distance is very far (untrustworthy)
+        if (numTags == 1 && avgDist > 4)
+            estStdDevs = VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
+        else
+            estStdDevs = estStdDevs.times(1 + (avgDist * avgDist / 30));
+
+        return estStdDevs;
+    }
+
+    // Calculate offset of robot to a specific april tag (x, y and angle)
+    public Optional<Pose2d> calculateOffsetToTag(int targetTagId) {
+        var result = camera.getLatestResult();
+        var targets = result.getTargets();
+
+        // Find the target April Tag from the seen targets
+        for (var target: targets) {
+            if (target.getFiducialId() == targetTagId) {
+                // Get the pose of the tag
+                var tagPose = poseEstimator.getFieldTags().getTagPose(target.getFiducialId());
+                if (tagPose.isPresent()) {
+                    AbstractMap.SimpleEntry<Optional<EstimatedRobotPose>, Pose3d> poseEntry = getEstimatedGlobalPose();
+
+                    // Extract
+                    Optional<EstimatedRobotPose> robotPose = poseEntry.getKey();
+                    Pose3d robot3dPose = poseEntry.getValue();
+
+                    if (robotPose.isPresent()) {
+                        // Convert Pose3d to Pose2d 
+                        Pose2d robot2dPose = new Pose2d(robot3dPose.getX(), robot3dPose.getY(), robot3dPose.getRotation().toRotation2d());
+                        
+                        // Calculate the offset (difference in position)
+                        Pose2d tagPosition = tagPose.get().toPose2d();
+                        double offsetX = robot2dPose.getX() - tagPosition.getX();
+                        double offsetY = robot2dPose.getY() - tagPosition.getY();
+                        double offsetAngle = robot2dPose.getRotation().getDegrees() - tagPosition.getRotation().getDegrees();
+
+                        // Return the offset as a new Pose2d
+                        return Optional.of(new Pose2d(offsetX, offsetY, Rotation2d.fromDegrees(offsetAngle)));
+                    }
+                }
+            }
+        }
+        // Return empty if tag not found
+        return Optional.empty();
+    }
+
+
+
+    
+}
